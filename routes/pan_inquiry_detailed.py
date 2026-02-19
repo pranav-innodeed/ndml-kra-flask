3926348563cfa43992e569ae88716766420d28c0
Venkteshwar Sahu <venktesh@LMP-Venkteshwar.local>
2026-02-19 11:53:18 +0530

    Minor changes

--- routes/pan_inquiry_detailed.py
+++ routes/pan_inquiry_detailed.py
@@ -59,11 +59,11 @@ def pan_inquiry(xml_bytes, enc_pwd):
     <soapenv:Header/>
     <soapenv:Body>
         <ns:panInquiryDetailsTwo>
-            <arg0><![CDATA[{xml_bytes}]]></arg0>
+            <arg0>{xml_bytes}</arg0>
             <arg1>{os.getenv("NDML_USER_ID")}</arg1>
             <arg2>{enc_pwd}</arg2>
             <arg3>{os.getenv("NDML_PASSKEY")}</arg3>
-            <arg4>{os.getenv("BP_ID")}</arg4>
+
         </ns:panInquiryDetailsTwo>
     </soapenv:Body>
     </soapenv:Envelope>
